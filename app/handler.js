5d546e62ee5b3f8aba8eea0ceaeb3043e9f1d8e3
Leyla Horne <lhorne@gavant.com>
2020-10-09 15:07:52 -0400

    Updated pem handling

--- app/handler.js
+++ app/handler.js
@@ -1,20 +1,43 @@
 'use strict';
 const { NodeSSH } = require('node-ssh')
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const fs = require('fs');
+
+function saveS3ToFile (bucket, key, destPath) {
+  var params = {
+    Bucket: bucket,
+    Key: key
+  }
+  let file = fs.createWriteStream(destPath);
+
+  return new Promise((resolve, reject) => {
+    s3.getObject(params).createReadStream()
+      .on('end', () => {
+        return resolve(destPath);
+      })
+      .on('error', (error) => {
+        return reject(error);
+      }).pipe(file);
+  });
+};
 
 module.exports.handler = async event => {
   console.log('Running Stat Updater')
 
-  // TODO encrypt/decrypt process.env.PEM
+  let pemPath = `/tmp/${process.env.PEM_KEY}`;
+  await saveS3ToFile(process.env.S3_BUCKET, process.env.PEM_KEY, pemPath)
+
   const sshClient = new NodeSSH();
   const connection = await sshClient.connect({
     username: `${process.env.USERNAME}`,
     host: `${process.env.HOST}`,
-    privateKey: `${process.env.PEM}`
+    privateKey: pemPath
   });
 
   if (connection.isConnected()) {
     console.log('Connected')
-    await connection.execCommand(`sudo bash -x stat-update-utility.sh "${process.env.ENVIRONMENT}" ""`, { cwd:'/home/ubuntu/stat-update-cron' }).then(function(result) {
+    await connection.execCommand(`sudo bash -x stat-update-utility.sh "${process.env.ENVIRONMENT}" ""`, { cwd: '/home/ubuntu/stat-update-cron' }).then(function (result) {
       console.log('STDOUT: ' + result.stdout)
       console.log('STDERR: ' + result.stderr)
       console.log('CODE: ' + result.code)
